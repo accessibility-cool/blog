796a442135bdb97b7492ff42d080fff635c2aa93
Simon Phumin Schweikert <git@simonschweikert.com>
2024-08-03 16:54:31 +0200

    removed paths and unused info, added TODOs

---
+++ src/routes/accessibility-statement/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+import Callout from '$lib/components/Callout.svelte';
+</script>
+
+<svelte:head>
+	<title>Imprint</title>
+</svelte:head>
+
+<h1>Imprint</h1>
+
+<p>accessibility.cool blog starter</p>
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+import Callout from '$lib/components/Callout.svelte';
+</script>
+
+<svelte:head>
+	<title>Privacy Policy</title>
+</svelte:head>
+
+<h1>Privacy Policy</h1>
+
+<p>accessibility.cool blog starter</p>
