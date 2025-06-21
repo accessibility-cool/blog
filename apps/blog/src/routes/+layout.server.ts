0a8812bd9c96c8d1e6a67591ebd4f95742f2ba77
Simon Phumin Schweikert <git@simonschweikert.com>
2025-06-21 10:38:13 +0900

    reactivated setHeaders

--- apps/blog/src/routes/+layout.server.ts
+++ apps/blog/src/routes/+layout.server.ts
@@ -3,14 +3,14 @@ import type { LayoutServerLoad } from './$types';
 import { defaultMetaTags, getAbout, getHome } from '@a11y.cool/data';
 
 export const load: LayoutServerLoad = (async (event) => {
-	// event.setHeaders({
-	// 	'X-Frame-Options': 'DENY',
-	// 	'X-Content-Type-Options': 'nosniff',
-	// 	'Referrer-Policy': 'no-referrer-when-downgrade',
-	// 	'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
-	// 	'Cross-Origin-Opener-Policy': 'same-origin',
-	// 	'Permissions-Policy': 'camera=(), microphone=(), geolocation=()'
-	// });
+	event.setHeaders({
+		'X-Frame-Options': 'DENY',
+		'X-Content-Type-Options': 'nosniff',
+		'Referrer-Policy': 'no-referrer-when-downgrade',
+		'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+		'Cross-Origin-Opener-Policy': 'same-origin',
+		'Permissions-Policy': 'camera=(), microphone=(), geolocation=()'
+	});
 
 	const home = await getHome();
 	const about = await getAbout();
